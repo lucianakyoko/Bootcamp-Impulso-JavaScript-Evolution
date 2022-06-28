7b38eec98608b5108ac1a104a1cad44c9ad50d0e
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-06-28 16:07:13 -0300

    style(webpage/styles): create colors.js

---
+++ webpage/src/styles/colors.js
@@ -0,0 +1,15 @@
+export const colors = {
+  primaryColor100: '#FFFFFF',
+  primaryColor500: '#222325',
+  primaryColor900: '#000000',
+
+  highlightColor400: '#F84B15',
+  highlightColor500: '#DE2A10',
+
+  categoryColor1: '#0598C6',
+  categoryColor2: '#C60579',
+
+  statusColor1: '#E02A2A',
+  statusColor2: '#C6AF05',
+  statusColor3: '#05C669', 
+};
